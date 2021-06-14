122a4f09ca1f8804a7ac340db96ae9edd688a573
Johnnes Souza <jhonnesf5@gmail.com>
2021-06-13 21:39:11 -0300

    Added Exercises 1008 and 1009

---
+++ 01_Beginner/1008_Salary.py
@@ -0,0 +1,6 @@
+employee = int(input())
+hours = float(input())
+value = float(input())
+
+print('NUMBER =', employee)
+print(f'SALARY = U$ {hours * value:.2f}')
---
+++ 01_Beginner/1009_Salary_with_Bonus.py
@@ -0,0 +1,5 @@
+name = input()
+wage = float(input())
+sales = float(input())
+
+print(f'TOTAL = R$ {wage + sales * 0.15:.2f}')
